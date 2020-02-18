e693d75464bd43e79b4023b5161e61ade0f4f259
y.aleksieiev <Xaixaex0>
2020-02-18 11:51:55 +0200

    feat: return contacts fixes

--- src/components/SaveContacts/SaveContacts.tsx
+++ src/components/SaveContacts/SaveContacts.tsx
@@ -10,29 +10,31 @@ const SaveContacts: React.FC<ISaveContactsProps> = ({ addressInput, addressValue
   const [name, setName] = useState<string>('');
   const [address, setAddress] = useState<string>('');
 
-  const { setError } = useRootData(({ setError }) => ({
+  const { setError, setModal } = useRootData(({ setError, setModal }) => ({
     setError,
+    setModal,
   }));
 
   const handleSave = useCallback(() => {
     try {
       if ((address && name) || (addressValue && name)) {
         const contacts = JSON.parse(localStorage.getItem('contacts') || '[]');
+        if (addressValue) {
+          setAddress(addressValue);
+        }
         const isContact = contacts.find(
-          ({ address: contactAddress, name: contactName }) =>
-            contactAddress === (addressValue ? addressValue : address) || contactName === name,
+          ({ address: contactAddress, name: contactName }) => contactAddress === address || contactName === name,
         );
         if (!isContact) {
-          const newContacts = addressValue
-            ? JSON.stringify([{ addressValue, name }, ...contacts])
-            : JSON.stringify([{ address, name }, ...contacts]);
+          const newContacts = JSON.stringify([{ address, name }, ...contacts]);
           localStorage.setItem('contacts', newContacts);
         }
+        setModal('');
       }
     } catch (err) {
       err.name && err.message ? setError(`${err.name}:${err.message}`) : setError(DEFAULT_ERROR);
     }
-  }, [address, addressValue, name, setError]);
+  }, [address, addressValue, name, setError, setModal]);
 
   return (
     <>
--- src/components/Transaction/Transaction.tsx
+++ src/components/Transaction/Transaction.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Modal from '../Modal/Modal';
 import SaveContacts from '../SaveContacts/SaveContacts';
@@ -39,10 +39,13 @@ const Transaction: React.FC<ITransactionProps> = ({
   const { setExecuted, setHash } = useTransaction();
 
   const [token, setToken] = useState<string>('');
-  const [inputValue, setInputValue] = useState<number | string>('');
-  const [maxValue, setMaxValue] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<number>(0);
+  const [maxValue, setMaxValue] = useState<number>(balances?.length ? balances[0].balance : 0);
   const [value, setValue] = useState<string>(localStorage.getItem('walletName') || '');
   const [symbolName, setSymbolName] = useState<string>('');
+  const [unlockFau, setUnlockFau] = useState<boolean>(false);
+  const [selectedValue, setSelectedValue] = useState<any>();
+  const [isContactsListOpen, openContactsList] = useState<boolean>(false);
 
   const validateNumbers = e => {
     if (INPUT_VALIDATION.digits.test(e)) {
@@ -68,8 +71,27 @@ const Transaction: React.FC<ITransactionProps> = ({
     }
   }, [ethId, value]);
 
+  const handleSelect = useCallback(name => {
+    setSelectedValue(name);
+  }, []);
+
+  const handleClickOutside = useCallback(e => {
+    if (e.target.getAttribute('data-name')) {
+      e.stopPropagation();
+      openContactsList(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [handleClickOutside]);
+
   return (
     <>
+      <div data-name="modal-wrapper" className={`modal-wrapper ${isContactsListOpen ? 'open' : 'closed'}`}></div>
       <Modal visible={false} classSpecifier="add-contact" background={true} cancelAction={() => null}>
         <SaveContacts addressValue={addressValue} addressInput={false} />
       </Modal>
@@ -107,28 +129,38 @@ const Transaction: React.FC<ITransactionProps> = ({
                       <div className="currency-input-wrapper">
                         <input
                           placeholder="Ox address, ENS or contact name"
-                          value={walletAddress ? walletAddress : addressValue}
+                          value={addressValue}
                           onChange={onChangeAddress}
                           className="currency-input-address"
                         />
-                        <button className="add-contact-button-input" onClick={() => setModal('add-contact')}>
-                          <span></span>
-                          <p>Save</p>
-                        </button>
-                        <select
-                          className="contacts-selector"
-                          onChange={e => {
-                            setToken(e.toString());
-                            setMaxValue(+e.target.value);
-                          }}
-                        >
-                          {contacts?.length &&
-                            contacts.map(({ name, address }) => (
-                              <option key={address} value={name}>
-                                {name}
-                              </option>
-                            ))}
-                        </select>
+                        {(walletAddress || addressValue) && !walletAddress && (
+                          <button className="add-contact-button-input" onClick={() => setModal('add-contact')}>
+                            <span></span>
+                            <p>Save</p>
+                          </button>
+                        )}
+                        <div className="custom-selector">
+                          <div onClick={() => openContactsList(!isContactsListOpen)} className="custom-selector-title">
+                            {selectedValue ? <p>{selectedValue}</p> : <span></span>}
+                            <div></div>
+                          </div>
+                          <ul className={`custom-selector-list ${isContactsListOpen ? 'open' : 'closed'}`}>
+                            {contacts?.length &&
+                              contacts.map(({ name, address }) => (
+                                <li
+                                  className="custom-selector-list-item"
+                                  key={address}
+                                  onClick={() => {
+                                    handleSelect(name);
+                                    setWalletAddress(address);
+                                    openContactsList(false);
+                                  }}
+                                >
+                                  {name}
+                                </li>
+                              ))}
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </>
@@ -140,11 +172,14 @@ const Transaction: React.FC<ITransactionProps> = ({
                       placeholder="0.00"
                       className={'currency-input'}
                       onChange={e => {
-                        validateNumbers(e.target.value);
+                        validateNumbers(+e.target.value);
                         onChangeAmount(+e.target.value);
                       }}
                       value={inputValue}
                     />
+                    <button className="all-balance" onClick={() => setInputValue(maxValue)}>
+                      <span>+</span> All balance
+                    </button>
                     <select
                       className="currency-selector"
                       onChange={e => {
@@ -172,7 +207,18 @@ const Transaction: React.FC<ITransactionProps> = ({
                     </div>
                   )}
                 </div>
-                <button className="btn submit-button" onClick={() => transactionAction(token)}>
+                <div onClick={() => setUnlockFau(true)} className="fau-unlock-wrapper">
+                  <p>Fau tocken unlocked</p>
+                  <button className={`fau-unlock-tocken ${unlockFau}`}>
+                    <span className={`fau-unlock-tocken-circle ${unlockFau}`}></span>
+                  </button>
+                </div>
+                <button
+                  className={`btn submit-button ${unlockFau ? '' : 'disabled'}`}
+                  onClick={() => {
+                    unlockFau ? transactionAction(token) : console.log('fau token is locked');
+                  }}
+                >
                   {title}
                 </button>
                 <p key={maxValue} className="transaction-fee">
