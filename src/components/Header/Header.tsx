3483d100e99b5a42efd29629ea8838101f7c223d
kraftwerk28 <kefirchik3@gmail.com>
2020-03-25 17:36:49 +0200

    fix(header): library for QR gen

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,7 @@ import { useRootData } from '../../hooks/useRootData';
 import { HEADER_ITEMS } from '../../constants/header';
 
 import './Header.scss';
+import { QRCode } from '../QRCode/QRCode';
 
 const Header: React.FC = () => {
   const {
@@ -23,19 +24,20 @@ const Header: React.FC = () => {
     walletName,
     zkBalancesLoaded,
     zkWallet,
-  } = useRootData(({ provider, walletName, zkBalancesLoaded, zkWallet, ...rest }) => ({
-    provider: provider.get(),
-    walletName: walletName.get(),
-    zkBalancesLoaded: zkBalancesLoaded.get(),
-    zkWallet: zkWallet.get(),
-    ...rest,
-  }));
+  } = useRootData(
+    ({ provider, walletName, zkBalancesLoaded, zkWallet, ...rest }) => ({
+      provider: provider.get(),
+      walletName: walletName.get(),
+      zkBalancesLoaded: zkBalancesLoaded.get(),
+      zkWallet: zkWallet.get(),
+      ...rest,
+    }),
+  );
 
   const address = zkWallet?.address();
   const userName = localStorage.getItem(zkWallet ? address! : '');
   const history = useHistory();
 
-  const [inp, setInp] = useState('');
   const [isCopyModal, openCopyModal] = useState<boolean>(false);
   const [isChangeNameOpen, openChangeName] = useState<boolean>(false);
 
@@ -45,7 +47,9 @@ const Header: React.FC = () => {
     (e: React.MouseEvent) => {
       e.stopPropagation();
       if (navigator.userAgent.match(/ipad|iphone/i)) {
-        const input: any = document.getElementsByClassName('copy-block-input')[0];
+        const input: any = document.getElementsByClassName(
+          'copy-block-input',
+        )[0];
         const range = document.createRange();
         range.selectNodeContents(input);
         const selection = window.getSelection();
@@ -80,7 +84,12 @@ const Header: React.FC = () => {
   useEffect(() => {
     if (!provider || walletName !== 'Metamask') return;
     provider.on('accountsChanged', () => {
-      if (zkWallet && provider && zkWallet?.address().toLowerCase() !== provider.selectedAddress.toLowerCase()) {
+      if (
+        zkWallet &&
+        provider &&
+        zkWallet?.address().toLowerCase() !==
+          provider.selectedAddress.toLowerCase()
+      ) {
         sessionStorage.setItem('walletName', walletName);
         handleLogOut();
         const savedWalletName = sessionStorage.getItem('walletName');
@@ -92,68 +101,87 @@ const Header: React.FC = () => {
   }, [handleLogOut, provider, setWalletName, walletName, zkWallet]);
 
   return (
-    <div className="menu-wrapper">
-      <div className="menu-top">
-        <Link className="menu-logo" to="/"></Link>
+    <div className='menu-wrapper'>
+      <div className='menu-top'>
+        <Link className='menu-logo' to='/'></Link>
         {address && (
           <>
-            <button type="button" className="menu-wallet" onClick={() => setModal('wallet')}>
-              <p>{userName || `${address.slice(0, 11)}...${address.slice(-4)}`}</p>
-              <img src={zkWallet ? makeBlockie(address) : avatar} alt="avatar" />
-              <div className="arrow-select"></div>
+            <button
+              type='button'
+              className='menu-wallet'
+              onClick={() => setModal('wallet')}
+            >
+              <p>
+                {userName || `${address.slice(0, 11)}...${address.slice(-4)}`}
+              </p>
+              <img
+                src={zkWallet ? makeBlockie(address) : avatar}
+                alt='avatar'
+              />
+              <div className='arrow-select'></div>
             </button>
-            <Modal visible={false} background={true} classSpecifier="wallet">
-              <div className="wallet-title">
-                <img src={zkWallet ? makeBlockie(address) : avatar} alt="avatar" />{' '}
+            <Modal visible={false} background={true} classSpecifier='wallet'>
+              <div className='wallet-title'>
+                <img
+                  src={zkWallet ? makeBlockie(address) : avatar}
+                  alt='avatar'
+                />{' '}
                 {userName || `${address.slice(0, 8)}...${address.slice(-4)}`}
               </div>
-              <div onClick={handleCopy} className="copy-block">
+              <div onClick={handleCopy} className='copy-block'>
                 <div className={`hint-copied ${isCopyModal ? 'open' : ''}`}>
                   <p>Copied!</p>
                 </div>
-                <input readOnly className="copy-block-input" value={address.toString()} ref={e => inputRef.push(e)} />
+                <input
+                  readOnly
+                  className='copy-block-input'
+                  value={address.toString()}
+                  ref={e => inputRef.push(e)}
+                />
                 <p>{address}</p>
-                <button className="copy-block-button" onClick={handleCopy} />
+                <button className='copy-block-button' onClick={handleCopy} />
               </div>
-              <div className="wallet-qr">
-                <img src={`https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${address}`} alt="wallet-qr" />
-              </div>
-              <div className="horizontal-line"></div>
-              <div className="wallet-buttons">
+              <QRCode data={address} />
+              <div className='horizontal-line'></div>
+              <div className='wallet-buttons'>
                 <button onClick={() => setModal('qr')}>
-                  <span className="icon-qr"></span>Show QR code
+                  <span className='icon-qr'></span>Show QR code
                 </button>
-                <div className="horizontal-line"></div>
+                <div className='horizontal-line'></div>
                 <button onClick={() => openChangeName(true)}>
-                  <span className="icon-edit"></span>Rename wallet
+                  <span className='icon-edit'></span>Rename wallet
                 </button>
-                <div className="horizontal-line"></div>
+                <div className='horizontal-line'></div>
                 <button onClick={() => handleLogOut()}>
-                  <span className="icon-disconnect"></span>Disconnect wallet
+                  <span className='icon-disconnect'></span>Disconnect wallet
                 </button>
-                <div className="horizontal-line"></div>
-                <input type="text" value={inp} onChange={e => setInp(e.target.value)} />
+                <div className='horizontal-line'></div>
               </div>
             </Modal>
-            <Modal visible={false} background={true} classSpecifier="qr">
-              <img src={`https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${address}`} alt="wallet-qr" />
+            <Modal visible={false} background={true} classSpecifier='qr'>
+              <QRCode data={address} />
             </Modal>
             <Modal
               visible={isChangeNameOpen}
               cancelAction={() => openChangeName(false)}
               background={true}
-              classSpecifier="change-name"
+              classSpecifier='change-name'
             >
               <ChangeName setModalOpen={openChangeName} />
             </Modal>
           </>
         )}
       </div>
-      <div className="menu-routes">
+      <div className='menu-routes'>
         {address &&
           HEADER_ITEMS.map(({ title, link }) => (
-            <div className="menu-route-wrapper" key={title}>
-              <Link className={`menu-route ${link === window?.location.pathname ? 'active' : ''}`} to={link}>
+            <div className='menu-route-wrapper' key={title}>
+              <Link
+                className={`menu-route ${
+                  link === window?.location.pathname ? 'active' : ''
+                }`}
+                to={link}
+              >
                 {title}
               </Link>
             </div>
