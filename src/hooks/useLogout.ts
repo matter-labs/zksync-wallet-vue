1702587d196ac480ef3b18168ed76f39af83fc90
kraftwerk28 <kefirchik3@gmail.com>
2020-04-13 20:10:43 +0300

    fix: unused import

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,5 +1,5 @@
 import { useHistory } from 'react-router-dom';
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 
 import { useRootData } from 'hooks/useRootData';
 
