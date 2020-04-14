fb998d9b50280e4a3abdaa2a0d25e53b71af3ab6
kraftwerk28 <kefirchik3@gmail.com>
2020-04-14 19:22:12 +0300

    add: cookie accept

--- src/utils.ts
+++ src/utils.ts
@@ -36,3 +36,32 @@ export function getWalletNameFromProvider(): string | undefined {
     return 'localhost';
   }
 }
+
+export async function getConfirmationCount(
+  provider: Web3Provider,
+  txHash: string,
+) {
+  try {
+    const trx = await provider.getTransaction(txHash);
+    const currentBlock = await provider.getBlockNumber();
+
+    if (typeof trx.blockNumber === 'undefined') return 0;
+    return currentBlock - trx.blockNumber;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
+}
+
+export function getCookie(name: string) {
+  const m = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  if (m) return m[2];
+}
+
+export function setCookie(name: string, value, exp?: Date) {
+  let val = `${name}=${value}`;
+  if (exp) {
+    val += `; Expires=${exp.toUTCString()}`;
+  }
+  document.cookie = val;
+}
