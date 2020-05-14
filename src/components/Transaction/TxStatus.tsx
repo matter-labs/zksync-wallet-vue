3abfa45aaf75ac8d8b933d71f3cb115c3bfdf83b
kraftwerk28 <kefirchik3@gmail.com>
2020-05-14 13:24:50 +0300

    feat: trx status

--- src/components/Transaction/TxStatus.tsx
+++ src/components/Transaction/TxStatus.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import { Tx } from 'src/pages/Transactions';
+import { useStore } from 'src/store/context';
+import { observer } from 'mobx-react-lite';
 
 export interface PieProps {
   value: number;
@@ -17,19 +19,6 @@ export function getTxStatus(tx: Tx) {
   return `${confirmCount}/${MAX_CONFIRM} confirmations`;
 }
 
-export function getPieProps(tx: Tx) {
-  const { commited, verified, confirmCount } = tx;
-  if (commited && !verified)
-    return { color: '#ff0', value: 1, status: 'Commited & unverified' };
-
-  if (verified) return { color: '#0f0', value: 1, status: 'Verified' };
-
-  return {
-    value: confirmCount / MAX_CONFIRM,
-    status: `${confirmCount}/${MAX_CONFIRM} confirmations`,
-  };
-}
-
 const percentCoords = percent =>
   [Math.cos(2 * Math.PI * percent), Math.sin(2 * Math.PI * percent)].join(' ');
 
@@ -37,6 +26,8 @@ const getPieChartD = percent =>
   `M ${percentCoords(0)} A 1 1 0 ${percent > 0.5 ? 1 : 0} 1 ${percentCoords(
     percent,
   )} L 0 0`;
+
+const PRIMARY_COLOR = '#8C8DFC';
 
 const Wrapper = ({ children, status }) => (
   <div className='tx-status' title={status}>
@@ -46,33 +37,57 @@ const Wrapper = ({ children, status }) => (
   </div>
 );
 
-export const TxStatus: FC<{ tx: Tx }> = ({ tx }) => {
-  const status = getTxStatus(tx);
-  const val = tx.confirmCount / (MAX_CONFIRM || 1);
+const CheckMark = () => (
+  <path
+    stroke={PRIMARY_COLOR}
+    strokeWidth='0.4'
+    strokeLinecap='round'
+    fill='transparent'
+    style={{ transform: 'rotate(90deg)' }}
+    d='M -0.7 0.2 L 0 0.8 L 0.9 -0.4'
+  />
+);
 
-  // Chechbox
-  if (tx.verified || tx.tx.type !== 'Deposit') {
-    return (
-      <Wrapper status={status}>
-        <path
-          stroke='#8C8DFC'
-          strokeWidth='0.4'
-          strokeLinecap='round'
-          fill='transparent'
-          style={{ transform: 'rotate(90deg)' }}
-          d='M -0.7 0.2 L 0 0.8 L 0.9 -0.4'
-        />
-      </Wrapper>
-    );
+const Clock = () => (
+  <g style={{ transform: 'rotate(90deg)' }}>
+    <circle
+      fill='transparent'
+      cx='0'
+      cy='0'
+      r='0.8'
+      stroke={PRIMARY_COLOR}
+      strokeWidth='0.3'
+    />
+    <path
+      d='M 0 0 L 0.3 -0.2 M 0 0 L 0 -0.4'
+      stroke={PRIMARY_COLOR}
+      strokeWidth='0.2'
+      strokeLinecap='round'
+    />
+  </g>
+);
+
+export const TxStatus: FC<{ tx: Tx }> = observer(({ tx }) => {
+  const store = useStore();
+  const isZkSync = tx.hash.startsWith('sync-tx');
+  const status = getTxStatus(tx);
+  const val = tx.confirmCount / (store.maxConfirmAmount || 1);
+
+  let content: JSX.Element | null = null;
+
+  if ((isZkSync && tx.commited && !tx.verified) || (!isZkSync && val > 1)) {
+    content = <Clock />;
+  } else if (tx.verified || tx.tx.type !== 'Deposit') {
+    content = <CheckMark />;
+  } else {
+    // Piechart
+    content =
+      val >= 1 ? (
+        <circle fill={PRIMARY_COLOR} cx='0' cy='0' r='1' />
+      ) : (
+        <path fill={PRIMARY_COLOR} d={getPieChartD(val)} />
+      );
   }
 
-  return (
-    <Wrapper status={status}>
-      {val >= 1 ? (
-        <circle fill='#8c8dfc' cx='0' cy='0' r='1' />
-      ) : (
-        <path fill='#8c8dfc' d={getPieChartD(val)} />
-      )}
-    </Wrapper>
-  );
-};
+  return <Wrapper status={status}>{content}</Wrapper>;
+});
