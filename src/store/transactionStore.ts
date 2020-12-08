94a80dd4b2967106be00ce528866aa2bb2b05e77
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2020-12-08 16:07:55 +0200

    fix: refactor copy-paste from zksync

--- src/store/transactionStore.ts
+++ src/store/transactionStore.ts
@@ -1,8 +1,8 @@
 import { ContractTransaction, ethers, BigNumberish } from 'ethers';
 import { action, observable } from 'mobx';
 import { LINKS_CONFIG, RESTRICTED_TOKENS } from 'src/config';
-import BigNumber from 'bignumber.js';
-import { Wallet, Provider, types as SyncTypes } from 'zksync';
+import { Wallet } from 'zksync';
+import { Transaction } from './zkSync/Transaction';
 
 export class TransactionStore {
   @observable recepientAddress = '';
@@ -96,83 +96,8 @@ export class TransactionStore {
   }
 }
 
-/*
-
-These classes are private in zksync.js and we've got no choice,
-but to copy-paste them
-
-*/
-
-class ZKSyncTxError extends Error {
-  constructor(
-    message: string,
-    public value:
-      | SyncTypes.PriorityOperationReceipt
-      | SyncTypes.TransactionReceipt,
-  ) {
-    super(message);
-  }
-}
-
-class Transaction {
-  state: 'Sent' | 'Committed' | 'Verified' | 'Failed';
-  error?: ZKSyncTxError;
-
-  constructor(
-    public txData: any,
-    public txHash: string,
-    public sidechainProvider: Provider,
-  ) {
-    this.state = 'Sent';
-  }
-
-  async awaitReceipt(): Promise<SyncTypes.TransactionReceipt> {
-    this.throwErrorIfFailedState();
-
-    // Got absolutely no idea how it compiled in zksync.js
-    /* @ts-ignore */
-    if (this.state !== 'Sent') return;
-
-    const receipt = await this.sidechainProvider.notifyTransaction(
-      this.txHash,
-      'COMMIT',
-    );
-
-    if (!receipt.success) {
-      this.setErrorState(
-        new ZKSyncTxError(
-          `zkSync transaction failed: ${receipt.failReason}`,
-          receipt,
-        ),
-      );
-      this.throwErrorIfFailedState();
-    }
-
-    this.state = 'Committed';
-    return receipt;
-  }
-
-  async awaitVerifyReceipt(): Promise<SyncTypes.TransactionReceipt> {
-    await this.awaitReceipt();
-    const receipt = await this.sidechainProvider.notifyTransaction(
-      this.txHash,
-      'VERIFY',
-    );
-
-    this.state = 'Verified';
-    return receipt;
-  }
-
-  private setErrorState(error: ZKSyncTxError) {
-    this.state = 'Failed';
-    this.error = error;
-  }
-
-  private throwErrorIfFailedState() {
-    if (this.state === 'Failed') throw this.error;
-  }
-}
-
+// This function is private in zkSync and thus
+// had to be copy-pasted
 async function setRequiredAccountIdFromServer(
   wallet: Wallet,
   actionName: string,
