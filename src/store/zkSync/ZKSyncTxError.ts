8a37368e58e7ad32886fba7e491acdc285996c3a
Serge Beresnev | Keriat <s.beresnev@matterlabs.dev>
2020-12-24 18:37:56 +0200

    refactor: feature merged into the production

--- src/store/zkSync/ZKSyncTxError.ts
+++
@@ -1,9 +0,0 @@
-import { types as SyncTypes } from 'zksync';
-
-// The class is private in zkSync and we've got
-// no choice but to copy-paste it
-export class ZKSyncTxError extends Error {
-  constructor(message: string, public value: SyncTypes.PriorityOperationReceipt | SyncTypes.TransactionReceipt) {
-    super(message);
-  }
-}
