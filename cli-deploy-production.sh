061ff0d4d9c5fe948a256d27b4ab42291beaea6e
Alex Gluchowski <ag@matterlabs.dev>
2020-12-27 11:48:53 +0000

    prod deployment scripts fixed and working

--- cli-deploy-production.sh
+++ cli-deploy-production.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+git pull &&
+git diff --quiet HEAD || (echo "There are uncommitted changes in the branch" && exit 1)
+
 yarn ci &&
 sh cli-process-env.sh "mainnet" &&
-yarn generate --fail-on-error
-firebase use zksync-vue-mainnet
-firebase deploy --only hosting
+yarn generate --fail-on-error &&
+firebase deploy -P zksync-vue-mainnet --only hosting &&
+echo deployed successfully!
