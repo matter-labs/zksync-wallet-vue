c3f74c7b7208676ab579f3d091f762dbd215fa1f
Serge Beresnev | Keriat <s.beresnev@matterlabs.dev>
2020-12-28 18:21:23 +0200

    hotfixes: after checking the Sentry
    — captcha removed
    — footer commented removed

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -8,32 +8,6 @@
     </div>
     <cookie-component />
     <footer-component/>
-<!--    <footer>-->
-<!--      <i-container>-->
-<!--        <i-row center class="_margin-top-1">-->
-<!--          <div class="_padding-x-1">-->
-<!--            <a target="_blank" class="footer-link link" href="//zksync.io/faq/intro.html">Docs</a>-->
-<!--          </div>-->
-<!--          <div class="_padding-x-1">-->
-<!--            <a target="_blank" class="footer-link" href="https://zksync.io/legal/terms.html">Terms of Use</a>-->
-<!--          </div>-->
-<!--          <div class="_padding-x-1">-->
-<!--            <a target="_blank" class="footer-link" href="https://zksync.io/legal/privacy.html">Privacy Policy</a>-->
-<!--          </div>-->
-<!--          <div class="_padding-x-1">-->
-<!--            <a target="_blank" class="footer-link" href="//zksync.io/contact.html">Contact</a>-->
-<!--          </div>-->
-<!--        </i-row>-->
-<!--
-<!--        <i-row center>-->
-<!--          <i-button block size="lg" circle @click="toggleDarkMode">-->
-<!--            <i-icon icon="light"/>-->
-<!--          </i-button>-->
-<!--        </i-row>-->
-
-<!--      </i-container>-->
-<!--    </footer>-->
-
   </div>
 </template>
 
