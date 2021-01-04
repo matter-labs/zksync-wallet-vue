4ccc9686d6e42dec0d69dcb35ad0e7c56f6c90ff
Serge Beresnev | Keriat <s.beresnev@matterlabs.dev>
2021-01-04 17:59:07 +0200

    hotfix: custom text in onboard-js changeable
    Fix for the task: https://linear.app/matterlabs/issue/ZKF-230/make-custom-text-in-onboard-js-changeable

--- src/plugins/onboardConfig.js
+++ src/plugins/onboardConfig.js
@@ -3,6 +3,7 @@ import web3Wallet from "@/plugins/web3.js";
 import { ETHER_NETWORK_ID, ETHER_NETWORK_NAME } from "@/plugins/build";
 
 const APP_NAME = "zkSync Beta";
+const EXPLANATION_LINK = "https://zksync.io/faq/wallets.html#what-if-my-wallet-is-not-supported-or-can-t-sign-a-message";
 const FORTMATIC_KEY = process.env.APP_FORTMATIC;
 const INFURA_KEY = process.env.APP_WALLET_CONNECT;
 const RPC_URL = `https://${ETHER_NETWORK_NAME}.infura.io/v3/${process.env.APP_WS_API_ETHERSCAN_TOKEN}`;
@@ -77,6 +78,13 @@ export default (ctx) => {
     },
     walletSelect: {
       wallets: initializedWallets.wallets,
+    },
+    popupContent: {
+      dismiss: "Dismiss",
+      teaser: "Can't find your wallet?",
+      fullHtml: `If you have funds in zkSync on an address that you can't control (a smart contract or an  exchange deposit account),
+        it is possible to initiate a forced withdrawal —&nbsp;<a href="${EXPLANATION_LINK}" target="_blank">please consult the documentation</a>.
+         In the future, this functionality will be automated.`,
     },
   };
 };
