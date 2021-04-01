5a0faa0181307bcbe2786a7cd7a2108b23255b9f
Serge Beresnev <keriat@me.com>
2021-04-01 07:44:56 +0300

    feat(plugins): Self-written code switched for the momentum library

--- src/plugins/main.ts
+++ src/plugins/main.ts
@@ -4,6 +4,7 @@ import utils from "@/plugins/utils";
 import Loader from "@/components/loader.vue";
 import { TokenSymbol } from "@/plugins/types";
 import { BigNumber } from "ethers";
+import moment from "moment";
 
 Vue.use(VueScrollTo);
 
@@ -24,11 +25,13 @@ Vue.filter("formatUsdAmount", (value: string | BigNumber, price: string, symbol:
 });
 
 /**
- * Filtering human-readable time
+ * Format date as a human-readable "XX ago"
  */
-Vue.filter("getTimeString", (value: number) => {
-  const { hours, minutes, seconds } = utils.timeCalc(value);
-  return `${hours ? utils.handleTimeAmount(hours, "hour") : ""}
-              ${minutes ? utils.handleTimeAmount(minutes, "minute") : ""}
-              ${seconds ? utils.handleTimeAmount(seconds, "second") : ""}`;
-});
+Vue.filter("formatTimeAgo", (time: moment.Moment | Date | string | number | (number | string)[] | moment.MomentInputObject | null | undefined) => moment(time).fromNow());
+
+/**
+ * Format date as a human-readable "XX ago"
+ */
+Vue.filter("formatDateTime", (time: moment.Moment | Date | string | number | (number | string)[] | moment.MomentInputObject | null | undefined) =>
+  moment(time).format("M/D/YYYY h:mm:ss A"),
+);
