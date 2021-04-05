c3cbcd7ada78dff43da9384d5b140a8c1a29511e
Serge Beresnev <keriat@me.com>
2021-04-05 13:42:00 +0300

    build(Build): Config filtes & packages updated

--- cli-process-env.sh
+++ cli-process-env.sh
@@ -4,32 +4,36 @@ NETWORK="$1"
 ENV_VALUE="$2"
 FORCE_REWRITE="$3"
 
-if [ -z "$NETWORK" ]; then
-  NETWORK="mainnet"
+echo "$FORCE_REWRITE"
+
+if [[ -z "$NETWORK" ]]; then
+  NETWORK="rinkeby"
 fi
 
-if [ -z "$ENV_VALUE" ]; then
+if [[ -z "$ENV_VALUE" ]]; then
   ENV_VALUE="dev"
-fi
-
-if [ -z "$FORCE_REWRITE" ]; then
-  SKIP=true
-else
-  SKIP=false
 fi
 
 FILE=".env"
 
-echo "CLI: Checking env... \r\n"
-if [ -f "$FILE" ]; then
+echo "Configure..."
+echo "- Environment: $ENV_VALUE"
+echo "- Ethereum network: $NETWORK"
+echo "- Generate .env anyway : $FORCE_REWRITE"
+echo ""
+
+echo "CLI: Checking env..."
+echo ""
+if [ -e "$FILE" ]; then
   echo ".env found"
 else
-  echo "setting “rinkeby” .env"
-  SKIP=false
+  echo "setting $NETWORK .env"
+  FORCE_REWRITE=1
 fi
 
-if [ SKIP ]; then
-  exit 0;
+if [ -z $FORCE_REWRITE ]; then
+  echo "Exiting without change..."
+  exit
 fi
 
 GIT_VERSION="APP_GIT_VERSION=$(git tag -l | tail -n1)"
@@ -37,8 +41,11 @@ GIT_REVISION="APP_GIT_REVISION=$(git rev-parse --short HEAD)"
 GIT_UPDATED_AT="APP_GIT_UPDATED_AT=\"$(git log -1 --format=%cd)\""
 APP_ENV="APP_ENV=$ENV_VALUE"
 
-rm -f .env src/.env &&
+rm -f ./.env &&
   cp "environments/.env.$NETWORK" ".env" &&
   echo "$GIT_VERSION" >>".env" &&
   echo "$GIT_REVISION" >>".env" &&
+  echo "$APP_ENV" >>".env" &&
   echo "$GIT_UPDATED_AT" >>".env"
+
+echo "Configured successfully"
