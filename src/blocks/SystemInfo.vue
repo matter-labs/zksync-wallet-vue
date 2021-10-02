1d3128a8a315a190b83fbcc6db3b4eb0052f411c
Sergey Beresnev <s.beresnev@matterlabs.dev>
2021-10-02 19:55:59 +0300

    ui(footer): Beta tag dropped

--- src/blocks/SystemInfo.vue
+++ src/blocks/SystemInfo.vue
@@ -18,7 +18,6 @@
         </span>
       </template>
     </i-popover>
-    <div class="beta-tag errorText">BETA</div>
     <a :href="githubLink" class="revision _background-gray-40" target="_blank">
       <v-icon name="ri-github-fill" />
       {{ revision }}
