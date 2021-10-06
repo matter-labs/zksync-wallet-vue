fb7a66095a3adcfe061f0d5e64e5cda7df9ac6de
Jack Hamer <jackhamer0990@gmail.com>
2021-10-06 14:39:02 +0200

    Do not autoRefreshOnNetworkChange

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,6 @@
 /**
  * zkSync Types
  */
-import { providers } from "ethers";
-import { WalletLinkProvider } from "walletlink";
-import { Provider } from "zksync";
 import { accessorType } from "@/store";
 
 // All interfaces, mappings and specific types
@@ -19,17 +16,5 @@ declare module "vue/types/vue" {
 declare module "@nuxt/types" {
   interface NuxtAppOptions {
     $accessor: typeof accessorType;
-  }
-}
-
-/**
- * @author Serge B. | Matter Labs
- * Shims-declaration of the [window.ethereum] (possibly undefined)
- */
-declare global {
-  interface Window {
-    // @ts-ignore
-    ethereum: providers.BaseProvider | providers.ExternalProvider | providers.JsonRpcFetchFunc | WalletLinkProvider | undefined;
-    syncProvider?: Provider;
   }
 }
