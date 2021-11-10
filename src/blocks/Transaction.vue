1bf278496602cea9b0c0995f8c17c546cf920629
Andrii Drebot <a.drebot@gmail.com>
2021-11-10 19:26:18 +0200

    feat(service): deposit ramp integration
    Ramp integration for the rinkeby & mainnet, with the configuration stored in Environments

--- src/blocks/Transaction.vue
+++ src/blocks/Transaction.vue
@@ -31,6 +31,12 @@
         <div>{{ transactionActionName }}</div>
       </div>
 
+      <template v-if="type === 'Deposit'">
+        <div class="_padding-0 _display-flex _justify-content-end">
+          <buy-with-ramp class="_padding-y-0" />
+        </div>
+      </template>
+
       <template v-if="type === 'Transfer'">
         <div class="_padding-0 _display-flex _justify-content-end">
           <i-button data-cy="send_send_l1_button" class="_padding-y-0 send-link" link variant="" to="/transaction/withdraw">
