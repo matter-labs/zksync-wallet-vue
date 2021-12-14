a76e8c8bb51ac44fc6b709e838eb3971ad922075
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2021-12-14 14:55:54 +0200

    fix: zkScan mobile modal link (#184)
    Co-authored-by: Sergey Beresnev <538438+keriat@users.noreply.github.com>
    Co-authored-by: Andrii Drebot <a.drebot@gmail.com>

--- src/blocks/modals/FooterModal.vue
+++ src/blocks/modals/FooterModal.vue
@@ -76,7 +76,7 @@ export default Vue.extend({
       },
     },
     blockExplorerLink(): string {
-      return this.$store.getters["zk-onboard/config"].ethereumNetwork.explorer;
+      return this.$store.getters["zk-onboard/config"].zkSyncNetwork.explorer;
     },
     isDarkTheme(): boolean {
       return this.theme === "dark";
