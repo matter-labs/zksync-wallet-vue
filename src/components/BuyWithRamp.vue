048382345575f80e4b65a2cb3169d3d3cc337723
Jack Hamer <jackhamer0990@gmail.com>
2021-12-17 13:54:21 +0200

    chore: hide ramp for testnets

--- src/components/BuyWithRamp.vue
+++ src/components/BuyWithRamp.vue
@@ -22,7 +22,7 @@ export default Vue.extend({
       return this.$store.getters["zk-account/address"];
     },
     isRampSupported(): boolean {
-      return !!this.config;
+      return this.$store.getters["zk-provider/network"] === "mainnet" && !!this.config;
     },
   },
   methods: {
