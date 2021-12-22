765d3a3637cce46ff12191061a5cc067e6b8662a
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2021-12-22 14:54:22 +0200

    fix: show transfer warning for transferNFT (#196)

--- src/blocks/modals/TransferWarning.vue
+++ src/blocks/modals/TransferWarning.vue
@@ -3,23 +3,31 @@
     <template slot="header">Transfer warning</template>
     <div>
       <div class="_padding-bottom-1">
-        You are about to transfer money to an address that doesn't have a zkSync balance yet. The transfer will happen inside zkSync L2. If you want to move money from zkSync to
-        the mainnet, please use the
-        <nuxt-link to="/transaction/withdraw" @click.native="$accessor.closeActiveModal()">Withdraw</nuxt-link>
+        You are about to transfer {{ isNFT ? "NFT" : "money" }} to an address that doesn't have a zkSync balance yet. The transfer will happen inside zkSync L2. If you want to move
+        {{ isNFT ? "NFT" : "money" }} from zkSync to the mainnet, please use the
+        <nuxt-link :to="isNFT ? '/transaction/nft/withdraw' : '/transaction/withdraw'" @click.native="$accessor.closeActiveModal()">Withdraw{{ isNFT ? " NFT" : "" }}</nuxt-link>
         function instead.
       </div>
       <i-checkbox v-model="transferWithdrawWarningCheckmark">Do not show this again</i-checkbox>
-      <i-button block class="_margin-top-1" size="lg" variant="secondary" @click="proceed()">Transfer inside zkSync</i-button>
+      <i-button block class="_margin-top-1" size="lg" variant="secondary" @click="proceed()">Transfer{{ isNFT ? " NFT" : "" }} inside zkSync</i-button>
     </div>
   </i-modal>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropOptions } from "vue";
 
+import { ZkTransactionType } from "@matterlabs/zksync-nuxt-core/types";
 export const warningCanceledKey = "canceledTransferWithdrawWarning";
 export default Vue.extend({
   name: "TransferWarning",
+  props: {
+    type: {
+      required: false,
+      type: String,
+      default: "Transfer",
+    } as PropOptions<ZkTransactionType>,
+  },
   data() {
     return {
       transferWithdrawWarningCheckmark: false,
@@ -27,7 +35,7 @@ export default Vue.extend({
   },
   computed: {
     opened: {
-      set(val): void {
+      set(val: boolean): void {
         if (val === false) {
           this.$accessor.closeActiveModal();
         }
@@ -35,6 +43,9 @@ export default Vue.extend({
       get(): boolean {
         return this.$accessor.currentModal !== null && this.$accessor.currentModal === "TransferWarning";
       },
+    },
+    isNFT(): boolean {
+      return this.type?.includes("NFT");
     },
   },
   methods: {
