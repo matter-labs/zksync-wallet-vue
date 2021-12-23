69bf46702e8e5c5d1095339033ee48e94a47ebf3
andriidrebot <a.drebot@gmail.com>
2021-12-23 15:25:39 +0200

    fix: remove debug mode

--- src/plugins/analytics.ts
+++ src/plugins/analytics.ts
@@ -8,7 +8,7 @@ export type Analytics = {
 
 class MixpanelAnalytics implements Analytics {
   constructor(token: string) {
-    mixpanel.init(token, { debug: true });
+    mixpanel.init(token, { debug: false });
   }
 
   set(props: { [key: string]: string }): void {
