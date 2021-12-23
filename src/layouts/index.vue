e4c3be3c6f99e9eb674e7399adeb89fff8f83320
Andrii Drebot <a.drebot@gmail.com>
2021-12-23 15:27:09 +0200

    feat: add env property to mixpanel (#200)
    * feat: add env property to mixpanel
    
    * fix: track on index
    
    * fix: disable debug
    
    * fix: add types
    
    * fix: check if props for exist
    
    * fix: move analytics to mixin
    
    * fix: remove debug mode

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -17,10 +17,12 @@
 import Vue from "vue";
 import theme from "@matterlabs/zksync-nuxt-core/utils/theme";
 import SentyMixin from "./sentry.mixin";
+import AnalyticsMixin from "./analytics.mixin";
+
 import { GIT_REVISION_SHORT } from "@/utils/config";
 
 export default Vue.extend({
-  mixins: [SentyMixin],
+  mixins: [SentyMixin, AnalyticsMixin],
   data() {
     return {
       version: GIT_REVISION_SHORT,
