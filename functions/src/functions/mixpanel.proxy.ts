00aaa2d42ac86859218a40e6a956cbc40f685d4d
Jack Hamer <jackhamer0990@gmail.com>
2022-01-27 11:46:59 +0200

    fix: forward request to mixpanel

--- functions/src/functions/mixpanel.proxy.ts
+++ functions/src/functions/mixpanel.proxy.ts
@@ -2,18 +2,24 @@ import * as functions from "firebase-functions";
 /**
  * Proxy request to mixpanel api
  *
- * @link https://docs.banxa.com/docs/step-3-authentication#signature
  * @param request
  * @param response
  */
 export function mixpanelProxyFunction(request: functions.Request, response: functions.Response): void {
   const mixpanelURL = "https://api.mixpanel.com";
-  const headers = {'X-REAL-IP': request.ip}
+  let ip = request.ip;
+  if(request.headers['HTTP_X_FORWARDED_FOR']) {
+    ip = <string>request.headers['HTTP_X_FORWARDED_FOR'];
+  } else if(request.headers['HTTP_X_REAL_IP']) {
+    ip = <string>request.headers['HTTP_X_REAL_IP'];
+  }
+  
+  const Http = new XMLHttpRequest();
+  Http.setRequestHeader("X-REAL-IP", ip);
+  Http.open(request.method, mixpanelURL+request.path.replace("/tunnel", "")+"?"+request.query);
+  Http.send(request.body);
 
-  response.status(200).json({
-    request,
-    response,
-    requestKeys: Object.keys(request),
-    responseKeys: Object.keys(response),
-  }).end();
+  Http.onreadystatechange = () => {
+    response.status(Http.status).contentType(Http.responseType).set(Http.getAllResponseHeaders()).send(Http.responseText).end();
+  }
 }
