7819c420f3a1a43c243badcdf4297c813e8887e5
Sergey Beresnev <s.beresnev@matterlabs.dev>
2022-02-01 07:35:08 +0200

    chore: fix

--- functions/src/functions/sentry.tunnel.ts
+++ functions/src/functions/sentry.tunnel.ts
@@ -9,24 +9,50 @@ import fetch from "node-fetch";
  * @param {functions.Response} response
  * @return Promise<void>
  */
-export const sentryTunnelFunction = async (request: functions.Request, response: functions.Response): Promise<void> => {
+export const sentryTunnelFunction = (request: functions.Request, response: functions.Response): void => {
   const envelope = request.body;
 
   functions.logger.debug("envelope", envelope);
 
-  functions.logger.debug("config", functions.config());
+  functions.logger.debug("config", functions.config().sentry);
 
   const projectId = functions.config().tunnel.sentry.project_id;
   const host = functions.config().tunnel.sentry.project_host;
 
-  const sentryResponse = await fetch(`https://${host}/api/${projectId}/envelope/`, {
+  const [rawHeader, ...restPieces] = envelope.split("\n");
+
+  const header = JSON.parse(rawHeader);
+
+  // omitted: check DSN, host, project ID
+
+  const bodyEncoded = [
+    // HACK: Attempt to communicate the real client IP address to Sentry.
+    //       The `forwarded_for` field was deduced from a test in Sentry's
+    //       Relay (server) handling of the Envelope format,
+    //       cf. https://git.io/JPwWP
+    JSON.stringify({
+                     ...header, forwarded_for: typeof request.headers["x-forwarded-for"] === "string" ? request.headers["x-forwarded-for"] : request.socket.remoteAddress
+                   }), ...restPieces
+  ].join("\n");
+
+  fetch(`https://${host}/api/${projectId}/envelope/`, {
     method: "POST", body: envelope
+  })
+  .then((sentryResponse) => {
+    functions.logger.debug("sentry response", sentryResponse);
+
+    if (sentryResponse.status !== 200) {
+      throw new functions.https.HttpsError("internal", `Looks like there was a problem. Status Code: ${sentryResponse.status}`);
+    }
+    return sentryResponse.json();
+  })
+  .then((data) => {
+    functions.logger.debug("sentry json data", data);
+    response.status(200);
+    response.send(data);
+  })
+  .catch((err) => {
+    functions.logger.error("sentry error", err);
+    throw new functions.https.HttpsError("internal", err.toString());
   });
-
-  functions.logger.debug("sentry API response", sentryResponse);
-
-  const responseData = await sentryResponse.json();
-  functions.logger.debug("sentry API responded with:", responseData);
-  response.status(200);
-  response.json(responseData);
 };
