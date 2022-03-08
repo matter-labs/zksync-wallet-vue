01e1e8fcb226ad4beae1a47a24326d52b96b6bb5
BohdanStruk <struk349@gmail.com>
2022-03-08 14:36:39 +0200

    feat: redirect to testnet etherscan if used etherscan hypelink

--- src/blocks/Balances.vue
+++ src/blocks/Balances.vue
@@ -4,8 +4,8 @@
       <template slot="header">zkSync is a Layer-2 protocol</template>
       <p>
         Your zkSync balances live in a separate space called Layer-2 (L2 for short). You won’t see them on
-        <a href="https://rinkeby.etherscan.io/" rel="noopener noreferrer" target="_blank">etherscan.io</a> or in your Ethereum wallet, only in zkSync wallet and block explorer.
-        Nevertheless, balances in zkSync are as secure as if though they were in L1 (the Ethereum mainnet).
+        <a :href="etherscanLink" rel="noopener noreferrer" target="_blank">etherscan.io</a> or in your Ethereum wallet, only in zkSync wallet and block explorer. Nevertheless,
+        balances in zkSync are as secure as if though they were in L1 (the Ethereum mainnet).
         <a href="https://zksync.io/userdocs/security.html" target="_blank" rel="noopener noreferrer">Learn more.</a>
       </p>
       <p>You can move your balances <b>from L1</b> into zkSync by making a <nuxt-link class="logoLinkContainer" to="/transaction/deposit">Deposit</nuxt-link></p>
@@ -105,6 +105,12 @@ export default Vue.extend({
     };
   },
   computed: {
+    isRinkeby(): boolean {
+      return this.$store.getters["zk-provider/network"] === "rinkeby-beta";
+    },
+    etherscanLink(): string {
+      return this.isRinkeby ? "https://rinkeby.etherscan.io" : "https://etherscan.io";
+    },
     accountStateLoading(): boolean {
       return this.$store.getters["zk-account/accountStateLoading"];
     },
