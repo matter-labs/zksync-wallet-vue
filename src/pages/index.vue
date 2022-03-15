4ce19e9b2a03077c1166953b804e85fc22d7740a
Sergey Beresnev <s.beresnev@matterlabs.dev>
2022-03-15 05:15:16 +0200

    fix: code styled

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,13 +5,13 @@
       <div class="container-fluid _flex-direction-row _display-flex connections">
         <div data-cy="core_connect_wallet_button" class="tileContainer _margin-right-05 _margin-md-right-2 _text-center" @click="customWallet()">
           <div class="tile">
-            <img src="@/assets/imgs/wallets/external.png" alt="External"/>
+            <img src="@/assets/imgs/wallets/external.png" alt="External" />
           </div>
           <div class="tileName">Ethereum Wallet</div>
         </div>
         <div data-cy="core_connect_wallet_wc_button" class="tileContainer _margin-left-05 _margin-md-left-2 _text-center" @click="walletConnect()">
           <div class="tile">
-            <img src="@/assets/imgs/wallets/wc.png" alt="Wallet Connect"/>
+            <img src="@/assets/imgs/wallets/wc.png" alt="Wallet Connect" />
           </div>
           <div class="tileName">Wallet Connect</div>
         </div>
@@ -20,61 +20,51 @@
         <h2 class="_display-flex _margin-y-0 _align-items-center">
           Can't find your wallet?
           <span class="icon-container _display-flex" @click="$accessor.openModal('AlternativeWithdrawal')">
-            <v-icon id="questionMark" name="ri-question-mark" class="iconInfo" :scale="0.9"/>
+            <v-icon id="questionMark" name="ri-question-mark" class="iconInfo" :scale="0.9" />
           </span>
         </h2>
         <h3 class="noteContainer">Or you’ve receive funds on your exchange address such as Binance?</h3>
-        <a
-          data-cy="core_connect_wallet_button"
-          class="tileContainer _margin-right-05 _margin-md-right-2 _text-center"
-          href="https://withdraw.zksync.io"
-          target="_blank"
-        >
+        <a data-cy="core_connect_wallet_button" class="tileContainer _margin-right-05 _margin-md-right-2 _text-center" href="https://withdraw.zksync.io" target="_blank">
           <div class="tile">
-            <img src="@/assets/imgs/logos/symbol.svg" alt="Alternative withdraw"/>
+            <img src="@/assets/imgs/logos/symbol.svg" alt="Alternative withdraw" />
           </div>
           <div class="tileName">Alternative Withdraw</div>
         </a>
       </div>
     </i-container>
-    <block-modals-alternative-withdraw/>
+    <block-modals-alternative-withdraw />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
+import Vue from "vue";
 
 export default Vue.extend({
   layout: "index",
-  data() {
-    return {
-      contactInfoShown: false
-    }
-  },
   mounted() {
     this.$analytics.track("visit_login");
   },
   methods: {
     async customWallet() {
-      const refreshWalletTry = await this.$store.dispatch("zk-onboard/loginWithOnboard")
+      const refreshWalletTry = await this.$store.dispatch("zk-onboard/loginWithOnboard");
       if (!refreshWalletTry) {
-        await this.$store.dispatch("zk-account/logout")
+        return this.$store.dispatch("zk-account/logout");
       } else {
-        this.$analytics.track("login", { connectionType: "Ethereum Wallet", wallet: this.$store.getters["zk-onboard/selectedWallet"] })
-        await this.$router.push("/account")
+        this.$analytics.track("login", { connectionType: "Ethereum Wallet", wallet: this.$store.getters["zk-onboard/selectedWallet"] });
+        return this.$router.push("/account");
       }
     },
     async walletConnect() {
-      const refreshWalletTry = await this.$store.dispatch("zk-onboard/loginWithWalletConnect")
+      const refreshWalletTry = await this.$store.dispatch("zk-onboard/loginWithWalletConnect");
       if (!refreshWalletTry) {
-        await this.$store.dispatch("zk-account/logout")
+        return this.$store.dispatch("zk-account/logout");
       } else {
-        this.$analytics.track("login", { connectionType: "WalletConnect", wallet: this.$store.getters["zk-onboard/selectedWallet"] })
-        await this.$router.push("/account")
+        this.$analytics.track("login", { connectionType: "WalletConnect", wallet: this.$store.getters["zk-onboard/selectedWallet"] });
+        return this.$router.push("/account");
       }
-    }
-  }
-})
+    },
+  },
+});
 </script>
 <style lang="scss" scoped>
 .indexPage {
@@ -236,7 +226,6 @@ export default Vue.extend({
 
   @media (max-height: 37rem) {
     .container {
-
       h1 {
         line-height: 35px;
         font-size: 22px;
@@ -271,6 +260,4 @@ export default Vue.extend({
     }
   }
 }
-
-
 </style>
