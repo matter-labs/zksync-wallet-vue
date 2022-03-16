1e5d7e922e595b80e7eb4404d00f2f10d2e8997d
Sergey Beresnev <538438+keriat@users.noreply.github.com>
2022-03-16 13:26:10 +0200

    fix: version link fixed (#233)
    Closes ZKF-1651

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ import { version as zkSyncVersion } from "zksync/package.json";
 import { version as walletVersion } from "../../package.json";
 
 export const GIT_REVISION: string = process.env.APP_GIT_REVISION ? process.env.APP_GIT_REVISION.toString() : "";
-export const GIT_REVISION_SHORT: string = GIT_REVISION ? GIT_REVISION.slice(-7) : "";
+export const GIT_REVISION_SHORT: string = GIT_REVISION.length > 8 ? GIT_REVISION.slice(0, 7) : GIT_REVISION;
 export const VERSION: string = walletVersion;
 export const ZK_LIB_VERSION: string = zkSyncVersion as string;
 
