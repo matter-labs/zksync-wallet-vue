6f01703a8fa94fd94508668647e24a591b2f91e7
Sergey Beresnev <s.beresnev@matterlabs.dev>
2022-03-21 05:24:04 +0200

    refactor: dropped unused computed & props of the v-icon

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -19,15 +19,8 @@ import theme from "@matterlabs/zksync-nuxt-core/utils/theme";
 import SentryMixin from "./sentry.mixin";
 import AnalyticsMixin from "./analytics.mixin";
 
-import { GIT_REVISION_SHORT } from "@/utils/config";
-
 export default Vue.extend({
   mixins: [SentryMixin, AnalyticsMixin],
-  data() {
-    return {
-      version: GIT_REVISION_SHORT,
-    };
-  },
   mounted() {
     this.$inkline.config.variant = theme.getUserTheme();
   },
