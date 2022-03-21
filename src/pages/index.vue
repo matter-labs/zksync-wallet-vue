9cba606648d131c9dc4104dd194514872724699c
Jack Hamer <jackhamer0990@gmail.com>
2022-03-21 18:18:14 +0200

    fix: show only argent when logging in with argent

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,7 @@
           </div>
           <div class="tileName">Wallet Connect</div>
         </button>
-        <button data-cy="core_connect_wallet_argent_button" class="tileContainer _margin-top-1 _margin-left-05 _margin-md-left-2 _text-center" @click="walletConnect()">
+        <button data-cy="core_connect_wallet_argent_button" class="tileContainer _margin-top-1 _margin-left-05 _margin-md-left-2 _text-center" @click="walletConnect(true)">
           <div class="tile">
             <img src="@/assets/imgs/wallets/argent.svg" alt="Argent" />
           </div>
@@ -60,8 +60,8 @@ export default Vue.extend({
         return this.$router.push("/account");
       }
     },
-    async walletConnect() {
-      const refreshWalletTry = await this.$store.dispatch("zk-onboard/loginWithWalletConnect");
+    async walletConnect(argent: boolean = false) {
+      const refreshWalletTry = await this.$store.dispatch(`zk-onboard/${argent ? 'loginWithArgent' : 'loginWithWalletConnect'}`);
       if (!refreshWalletTry) {
         return this.$store.dispatch("zk-account/logout");
       } else {
