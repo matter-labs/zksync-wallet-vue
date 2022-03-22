6b8283abed4dc248b67c699a2cc6a9ca1cc2d4a1
Sergey Beresnev <538438+keriat@users.noreply.github.com>
2022-03-23 01:11:23 +0200

    fix: icon (previously requested by the partners)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -118,7 +118,7 @@ export default Vue.extend({
         img {
           width: 100%;
           height: 100%;
-          object-fit: cover;
+          object-fit: contain;
           transition: $transition1;
         }
 
