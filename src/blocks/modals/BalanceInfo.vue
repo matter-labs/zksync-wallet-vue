ac96376b27e242fc3a019f8fbd59d06d5db21af3
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2023-02-01 14:45:37 +0100

    fix: renaming text changes (#321)

--- src/blocks/modals/BalanceInfo.vue
+++ src/blocks/modals/BalanceInfo.vue
@@ -2,23 +2,23 @@
   <i-modal v-model="opened" size="md">
     <template #header>zkSync is a Layer-2 protocol</template>
     <p>
-      Your zkSync balances live in a separate space called Layer-2 (L2 for short). You won’t see them on
+      Your zkSync lite balances live in a separate space called Layer-2 (L2 for short). You won’t see them on
       <a
         :href="`https://${etherscanDomain}/address/${$store.getters['zk-account/address']}`"
         rel="noopener noreferrer"
         target="_blank"
         >{{ etherscanDomain }}</a
       >
-      or in your Ethereum wallet, only in zkSync wallet and block explorer. Nevertheless, balances in zkSync are as
-      secure as if though they were in L1 (the Ethereum mainnet).
+      or in your Ethereum wallet, only in zkSync lite wallet and block explorer. Nevertheless, balances in zkSync lite
+      are as secure as if though they were in L1 (the Ethereum mainnet).
       <a href="https://zksync.io/userdocs/security.html" rel="noopener noreferrer" target="_blank">Learn more.</a>
     </p>
     <p>
-      You can move your balances <b>from L1</b> into zkSync by making a
+      You can move your balances <b>from L1</b> into zkSync lite by making a
       <nuxt-link class="logoLinkContainer" to="/transaction/deposit">Deposit</nuxt-link>
     </p>
     <p>
-      To move them back from zkSync <b>to L1</b> you can make a
+      To move them back from zkSync lite <b>to L1</b> you can make a
       <nuxt-link class="logoLinkContainer" to="/transaction/withdraw">Withdraw</nuxt-link>
     </p>
   </i-modal>
