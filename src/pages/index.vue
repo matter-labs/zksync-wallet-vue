d9b26e64498e76751eab293f548720a21b28d170
Bohdan Struk <97437702+bohdan-struk-matterlabs@users.noreply.github.com>
2023-03-20 10:06:35 -0500

    fix: headline text is overlapped with menu (#326)
    * fix: headline text is overlapped with menu
    
    * fix: headline text is overlapped with menu

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -99,7 +99,7 @@ export default Vue.extend({
 .indexPage {
   width: 100%;
   height: 100%;
-  padding-top: 60px;
+  padding-top: 93px;
   padding-bottom: 56px;
   display: flex;
   justify-content: center;
